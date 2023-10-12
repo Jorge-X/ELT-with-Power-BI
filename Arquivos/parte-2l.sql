e03e1da2ecec0f2b0b01b39802aa21fc7df75b03
Jorge Luis <140755201+Jorge-X@users.noreply.github.com>
2023-10-12 19:42:50 -0300

    Adiciona os arquivos utilizados no relatório
    Adiciona: parte-1, parte-2, Arquivo-do-Projeto.pbix

---
+++ Arquivos/parte-2l.sql
@@ -0,0 +1,80 @@
+-- Consultas SQL
+
+select * from employee;
+select Ssn, count(Essn) from employee e, dependent d where (e.Ssn = d.Essn);
+select * from dependent;
+
+SELECT Bdate, Address FROM employee
+WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+
+select * from departament where Dname = 'Research';
+
+SELECT Fname, Lname, Address
+FROM employee, departament
+WHERE Dname = 'Research' AND Dnumber = Dno;
+
+select * from project;
+--
+--
+--
+-- Expressões e concatenação de strings
+--
+--
+-- recuperando informações dos departamentos presentes em Stafford
+select Dname as Department, Mgr_ssn as Manager from departament d, dept_locations l
+where d.Dnumber = l.Dnumber;
+
+-- padrão sql -> || no MySQL usa a função concat()
+select Dname as Department, concat(Fname, ' ', Lname) from departament d, dept_locations l, employee e
+where d.Dnumber = l.Dnumber and Mgr_ssn = e.Ssn;
+
+-- recuperando info dos projetos em Stafford
+select * from project, departament where Dnum = Dnumber and Plocation = 'Stafford';
+
+-- recuperando info sobre os departamentos e projetos localizados em Stafford
+SELECT Pnumber, Dnum, Lname, Address, Bdate
+FROM project, departament, employee
+WHERE Dnum = Dnumber AND Mgr_ssn = Ssn AND
+Plocation = 'Stafford';
+
+SELECT * FROM employee WHERE Dno IN (3,6,9);
+
+--
+--
+-- Operadores lógicos
+--
+--
+SELECT Bdate, Address
+FROM EMPLOYEE
+WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+
+SELECT Fname, Lname, Address
+FROM employee, departament
+WHERE Dname = 'Research' AND Dnumber = Dno;
+--
+--
+-- Expressões e alias
+--
+--
+
+-- recolhendo o valor do INSS-*
+select Fname, Lname, Salary, Salary*0.011 from employee;
+select Fname, Lname, Salary, Salary*0.011 as INSS from employee;
+select Fname, Lname, Salary, round(Salary*0.011,2) as INSS from employee;
+
+-- definir um aumento de salário para os gerentes que trabalham no projeto associado ao ProdutoX
+select e.Fname, e.Lname, 1.1*e.Salary as increased_sal from employee as e,
+works_on as w, project as p where e.Ssn = w.Essn and w.Pno = p.Pnumber and p.Pname='ProductX';
+
+-- concatenando e fornecendo alias
+select Dname as Department, concat(Fname, ' ', Lname) as Manager from departament d, dept_locations l, employee e
+where d.Dnumber = l.Dnumber and Mgr_ssn = e.Ssn;
+
+-- recuperando dados dos empregados que trabalham para o departamento de pesquisa
+select Fname, Lname, Address from employee, departament
+	where Dname = 'Research' and Dnumber = Dno;
+
+-- definindo alias para legibilidade da consulta
+select e.Fname, e.Lname, e.Address from employee e, departament d
+	where d.Dname = 'Research' and d.Dnumber = e.Dno;
+
